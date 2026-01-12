b61a9be8308ec82c43add8ddf5509293c4c42e62
MyoThet <mr.myothet@gmail.com>
2026-01-12 15:52:29 +0700

    standard deviation and correlation

--- 1_Intro_to_Python/4_Numpy/Average_vs_Median.py
+++ 1_Intro_to_Python/4_Numpy/Average_vs_Median.py
@@ -11,3 +11,20 @@ np_height_in = np_baseball[:,0]
 
 print(np.mean(np_height_in))
 print(np.median(np_height_in))
+
+# Explore the baseball data
+
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height
+med = np.median(np_baseball[:, 0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height
+stddev = np.std(np_baseball[:, 0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column
+corr = np.corrcoef(np_baseball[:, 0], np_baseball[:, 1])
+print("Correlation: " + str(corr))
